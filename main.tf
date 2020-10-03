45e114b051b8515fc875b86cb01206c51279d8af
mazen-bassiouny <mazen.atef2012@gmail.com>
2020-10-03 11:48:10 +0200

    create container hello world

--- main.tf
+++ main.tf
@@ -8,9 +8,31 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "DevRG" {
   name     = "Dev10"
-  location = "eastus"
+  location = "westeurope"
 
   tags = {
     environment = "Dev"
+  }
+}
+
+
+resource "azurerm_container_group" "example" {
+  name                = "test-container-group"
+  location            = azurerm_resource_group.DevRG.location
+  resource_group_name = azurerm_resource_group.DevRG.name
+  ip_address_type     = "public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world"
+    image  = "microsoft/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 443,80
+      protocol = "TCP"
+    }
   }
 }
